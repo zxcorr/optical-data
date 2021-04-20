5769ffdbbbd34f23a05cd99fd25e5490dd011ee9
Rafael <ribeirorafaelmatheus@gmail.com>
2021-04-20 11:05:54 -0300

    Teste

--- Rafael/functions/metrics.py
+++ Rafael/functions/metrics.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 '''
-This script contains metrics to ensure the quality of photmoetric redshift estimation in this work
+This script contains metrics to ensure the quality of photmoetric redshift estimation in this work teste
 '''
 
 
